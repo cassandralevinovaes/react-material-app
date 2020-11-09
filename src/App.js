2194334f97e2f309cdfd4ca1109619f545c309c0
cassandralevinovaes <cassandralevi@gmail.com>
2020-11-08 22:34:43 -0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -13,6 +13,20 @@ const buttonStyle = {
       backgroundColor: '#f7dad9',
       letterSpacing: '2px'
 }
+const facebookStyle = {
+   width: '300px',
+      backgroundColor: '#3b5998',
+      color: '#fff',
+      textTransform:'capitalize',
+      letterSpacing: '1px'
+}
+const twitterStyle = {
+   width: '300px',
+      backgroundColor: '#00acee',
+      color: '#fff',
+      textTransform:'capitalize',
+      letterSpacing: '1px'
+}
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
@@ -31,13 +45,23 @@ export default function App() {
 
 <h1>React Material</h1>
 <p>Material UI for coding react app</p>
+<br />
+
+<p></p>
 
 <form className={classes.root} noValidate autoComplete="off">
   <TextField id="outlined-size-small" label="Email" variant="outlined" /><br />
    <TextField id="outlined-size-small" label="Password" variant="outlined" />
 </form>
 <Button style={buttonStyle} size='large' disableElevation color='default'> Get started</Button>
+
+<br />
+<p> OR </p>
    
+   <Button style={facebookStyle} size='large' disableElevation color='default'> Login with facebook</Button>
+   <p></p>
+   
+   <Button style={twitterStyle} size='large' disableElevation color='default'> Login with twitter</Button>
     </div>
   );
 }
